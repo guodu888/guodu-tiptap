76dfad6c68906c33307987691d199201423df604
shy1118 <shy1118999@qq.com>
2023-11-28 19:05:47 +0800

    chore: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import path from 'path'
+import path from 'node:path'
+import process from 'node:process'
 import { defineConfig, loadEnv } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
