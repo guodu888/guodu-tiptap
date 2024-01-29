927467eab6d806eec040885cf446b01dc172f74a
shy1118 <shy1118999@qq.com>
2024-01-29 15:35:35 +0800

    fix: eslint fix

--- shims.d.ts
+++ shims.d.ts
@@ -1,12 +1,14 @@
 import type { VirtualKeyboardInterface } from 'mathlive'
+
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
+
+  const component: DefineComponent<object, object, any>
   export default component
 }
 
 declare global {
   interface Window {
-      mathVirtualKeyboard: VirtualKeyboardInterface & EventTarget;
+    mathVirtualKeyboard: VirtualKeyboardInterface & EventTarget
   }
 }
